54e42fc8e5ea060adcd5923f02a5d970ef9ab7b8
hoovercj <hoovercj@iastate.edu>
2014-03-15 15:34:29 -0500

    added google play services stuff

--- Dingus-android/src/com/codeday/dingus/MainActivity.java
+++ Dingus-android/src/com/codeday/dingus/MainActivity.java
@@ -4,17 +4,75 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.example.games.basegameutils.GameHelper;
+import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
-public class MainActivity extends AndroidApplication
+import dingusInterfaces.ActionResolver;
+
+public class MainActivity extends AndroidApplication 
+			implements GameHelperListener, ActionResolver
 {
+	private GameHelper gameHelper;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+	
+		gameHelper = new GameHelper(this);
+        gameHelper.enableDebugLog(true, "GPGS");
+		
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = true;
 		config.useCompass = false;
 		config.useGL20 = true;
 		initialize(new Dingus(), config);
+	}
+	
+	@Override
+	public boolean getSignedInGPGS() {
+		return gameHelper.isSignedIn();
+	}
+
+	@Override
+	public void loginGPGS() {
+		try {
+			runOnUiThread(new Runnable(){
+				public void run() {
+					gameHelper.beginUserInitiatedSignIn();
+				}
+			});
+		} catch (final Exception ex) {
+		}
+	}
+
+	@Override
+	public void submitScoreGPGS(int score) {
+		gameHelper.getGamesClient().submitScore(getString(R.string.leaderboard_id), score);		
+	}
+
+	@Override
+	public void unlockAchievementGPGS(String achievementId) {
+		gameHelper.getGamesClient().unlockAchievement(achievementId);
+	}
+
+	@Override
+	public void getLeaderboardGPGS() {
+		startActivityForResult(gameHelper.getGamesClient().getLeaderboardIntent(getString(R.string.leaderboard_id)), 100);		
+	}
+
+	@Override
+	public void getAchievementsGPGS() {
+		startActivityForResult(gameHelper.getGamesClient().getAchievementsIntent(), 101);
+	}
+
+	@Override
+	public void onSignInFailed() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		// TODO Auto-generated method stub
 	}
 }
